36cd0482a51c21f38b213cbb45ff9630ffbe5b90
Jacob Zimmerman <jczimm@jczimm.com>
2019-03-12 14:19:34 -0400

    might the problem be that ncc can't static-analyze async createBrowser using IS_NOW?

--- puppeteer-helper.js
+++ puppeteer-helper.js
@@ -6,15 +6,17 @@ if (process.env.IS_NOW) {
   puppeteer = require('puppeteer');
 }
 
-async function createBrowser() {
+function createBrowser() {
   if (process.env.IS_NOW) {
-    return await puppeteer.launch({
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-    });
+    return chrome.executablePath.then((executablePath) =>
+      puppeteer.launch({
+        args: chrome.args,
+        executablePath,
+        headless: chrome.headless,
+      })
+    );
   } else {
-    return await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    return puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     // I'm not using a sandbox for puppeteer because I absolutely trust the content I'm viewing--it's all local
     // (not using a sandbox for pupeteer is almost always a bad practice and may be so even here)
   }
