f3f5d49d7fd9c5dff9e9d122ce9c93ebe25b60b5
Jacob Zimmerman <jczimm@jczimm.com>
2019-03-12 14:43:50 -0400

    revert now.json env edits to before breaking change, improve puppeteer-helper

--- puppeteer-helper.js
+++ puppeteer-helper.js
@@ -1,25 +1,18 @@
-let puppeteer, chrome;
-if (process.env.IS_NOW) {
-  puppeteer = require('puppeteer-core');
-  chrome = require('chrome-aws-lambda');
-} else {
-  puppeteer = require('puppeteer');
-}
+let createBrowser;
 
-function createBrowser() {
-  if (process.env.IS_NOW) {
-    return chrome.executablePath.then((executablePath) =>
-      puppeteer.launch({
-        args: chrome.args,
-        executablePath,
-        headless: chrome.headless,
-      })
-    );
-  } else {
-    return puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    // I'm not using a sandbox for puppeteer because I absolutely trust the content I'm viewing--it's all local
-    // (not using a sandbox for pupeteer is almost always a bad practice and may be so even here)
-  }
+if (process.env.IS_NOW) {
+  const puppeteer = require('puppeteer-core');
+  const chrome = require('chrome-aws-lambda');
+  createBrowser = async () => await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  });
+} else {
+  const puppeteer = require('puppeteer');
+  createBrowser = async () => await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  // I'm not using a sandbox for puppeteer because I absolutely trust the content I'm viewing--it's all local
+  // (not using a sandbox for pupeteer is almost always a bad practice and may be so even here)
 }
 
 module.exports = { createBrowser };
